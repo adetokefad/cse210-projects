eba02926fca96c824babf0ca89af8aa6435e47fc
adetokefad <adetokefaderin@gmail.com>
2025-02-01 02:30:02 +0100

    added code to fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()} = {fraction1.GetDecimalValue()}");
+        Console.WriteLine($"fraction 2: {fraction2.GetFractionString()} = {fraction2.GetDecimalValue()}");
+        Console.WriteLine($"fraction 3: {fraction3.GetFractionString()} = {fraction3.GetDecimalValue()}");
+
+        fraction1.Numerator = 3;
+        fraction1.Denominator = 4;
+
+        Console.WriteLine($"Updated Fraction 1: {fraction1.GetFractionString()} = {fraction1.GetDecimalValue()}");
     }
 }
